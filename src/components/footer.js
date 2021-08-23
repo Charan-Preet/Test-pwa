d54ff43d86bb86c25600b700cee32ffb179c7037
Charan-Preet <charanpreet@tuta.io>
2021-08-23 17:19:15 +0530

    first-commit

---
+++ src/components/footer.js
@@ -0,0 +1,15 @@
+import 'tachyons'
+
+export default function Footer() {
+    return (
+        <footer className="pv4 ph3 ph5-m ph6-l mid-gray">
+            <small className="f6 db tc">© 2016 <b className="ttu">SOME COMPANY Inc</b>., All Rights Reserved</small>
+            <div className="tc mt3">
+                <a href="/language/" title="Language" className="f6 dib ph2 link mid-gray dim">Language</a>
+                <a href="/terms/" title="Terms" className="f6 dib ph2 link mid-gray dim">Terms of Use</a>
+                <a href="/privacy/" title="Privacy" className="f6 dib ph2 link mid-gray dim">Privacy</a>
+            </div>
+        </footer>
+
+    )
+}
